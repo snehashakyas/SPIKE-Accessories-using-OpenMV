8c25b901b634f1132d2224b9bee96d7a5ea27ff1
snehashakyas <65065868+snehashakyas@users.noreply.github.com>
2021-04-07 19:24:06 -0400

    Update Color Sensor SPIKE.py

--- Color Sensor SPIKE.py
+++ Color Sensor SPIKE.py
@@ -4,6 +4,17 @@ hub.port.C.mode(hub.port.MODE_FULL_DUPLEX)
 hub.port.C.baud(9600)
 
 while True:
+    # Entered LAB values below by user.
+    L_val = "3"
+    A_val = "-5"
+    B_val = "0"
+    
+    inputVariable = L_val + " " + A_val + " " + B_val
+    
+    hub.port.C.write(inputVariable)
+    time.sleep(1)
+    
     a = hub.port.C.read(10)
-    print(a)
+    print(a) # Prints out if color indicated by user's LAB values have been detected.
+    
     time.sleep(1)
