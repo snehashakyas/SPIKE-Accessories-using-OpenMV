fa757956ff16e14c7056d6d4758d2b3e03678020
snehashakyas <65065868+snehashakyas@users.noreply.github.com>
2021-05-14 22:50:23 +0545

    Add files via upload

---
+++ Combined SPIKE.py
@@ -0,0 +1,21 @@
+import hub
+import time
+
+class OPENMV:
+    def __init__(self, port, baud=9600):
+        self.s=port
+        self.s.mode(hub.port.MODE_FULL_DUPLEX)
+        time.sleep(0.1)
+        self.s.baud(9600)
+        
+    def readColors(self):
+        self.s.write('$color')
+        time.sleep(0.2)
+        a = self.s.read(7)
+        print(a)
+              
+    def readFaceRecog(self):
+        self.s.write('$faces')
+        time.sleep(0.2)
+        b = self.s.read(10)
+        print(b)
