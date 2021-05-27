24cbc4e722c91cb0cdd6626a2a50c414fcb9c7fb
snehashakyas <65065868+snehashakyas@users.noreply.github.com>
2021-05-27 18:58:15 +0545

    Update ColorSensor Mode2 SPIKE.py

--- ColorSensor Mode2 SPIKE.py
+++ ColorSensor Mode2 SPIKE.py
@@ -1,7 +1,9 @@
 import hub
 import time
-hub.port.C.mode(hub.port.MODE_FULL_DUPLEX)
+hub.port.C.mode(hub.port.MODE_FULL_DUPLEX) # OpenMV connected to Port C in SPIKE.
 hub.port.C.baud(9600)
+
+a = hub.port.C.read(10) # To discard first read value, which is always '\xff'.
 
 while True:
     # Enter LAB values below, in between the apostrophes. For example, right now the LAB values correspond to 3, -5, and 0 respectively.
@@ -10,10 +12,12 @@ while True:
     B_val = "0"
 
     inputVariable = L_val + " " + A_val + " " + B_val
-
+    
+    # Current bug in code: the below line causes a number to be printed out in the SPIKE, which is unwanted. 
     hub.port.C.write(inputVariable)
-
+    
     a = hub.port.C.read(10)
-    print(a) # Prints if LAB values you indicate have been detected.
+    if(len(a) != 0):
+        print(a) # Prints if LAB values you indicate have been detected.
 
     time.sleep(1)
