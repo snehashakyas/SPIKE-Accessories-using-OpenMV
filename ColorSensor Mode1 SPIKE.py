1848aae69b97bfe88d0ee5a1c5f3105742a8b292
snehashakyas <65065868+snehashakyas@users.noreply.github.com>
2021-05-27 17:57:40 +0545

    Update ColorSensor Mode1 SPIKE.py

--- ColorSensor Mode1 SPIKE.py
+++ ColorSensor Mode1 SPIKE.py
@@ -3,8 +3,11 @@ import time
 hub.port.C.mode(hub.port.MODE_FULL_DUPLEX)
 hub.port.C.baud(9600)
 
+a = hub.port.C.read(10) # To discard first read value, which is always '\xff'.
+
 while True:   
     a = hub.port.C.read(10)
-    print(a) # Prints out color detected on SPIKE.
+    if(len(a) != 0):
+        print(a) # Prints out color detected on SPIKE.
     
     time.sleep(1)
